4bdf5bbf0f89359d872f50942e52b305368f07cf
Ronkkeli <petrilahtela1997@gmail.com>
2021-10-07 13:53:36 +0100

    Update

--- Run.bat
+++ Run.bat
@@ -1,4 +1,4 @@
-                                                                                                       
+git clone https://github.com/Ronkkeli/Suomalainen-Laskin.git                                                                                                       
 title Software Made by Ronkkeli                                                                
 pip --version
 pip install --upgrade pip
