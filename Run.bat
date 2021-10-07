d0a4a651ef625079f5075b7d7ecffbb9d8ace678
Ronkkeli <petrilahtela1997@gmail.com>
2021-10-07 13:51:33 +0100

    D

---
+++ Run.bat
@@ -0,0 +1,6 @@
+                                                                                                       
+title Software Made by Ronkkeli                                                                
+pip --version
+pip install --upgrade pip
+python3 main.py
+pause
